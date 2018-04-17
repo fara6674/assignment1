ce9ba9614d51fa76fe6972173a647bb7cd1130bc
fara6674 <farrukhbek@gmail.com>
2018-04-17 14:03:30 -0500

    first commit

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+
+public class Welcome {
+	
+	public static void main(String[] args) {
+	
+	System.out.println("My name is Farruh");
+	System.out.println("I'm 32 years old");
+	System.out.println("I live in Huntington Hill dr Ballwin MO 63021");
+
+}
+
+}
